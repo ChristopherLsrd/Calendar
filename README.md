c419615277308a764158764e720d96f0078e4354
Christopher <christopher.lessirard@gmail.com>
2023-04-08 15:28:34 +0200

    Follow tutorial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Calendar
+ Tutorial: https://www.youtube.com/watch?v=Q8NV4koY7nU
---
+++ server/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors= require("cors");
+
+
+const app= express();
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+//}, () => console.log("Connected to MongoDB"));
+});
+app.use("/api/calendar",require("./Controllers/CalendarController"));
+
+app.listen(5000,()=> console.log("Server started"));
+
